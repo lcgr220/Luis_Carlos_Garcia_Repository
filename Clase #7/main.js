9ec31ac0be2349987de325064317577d04c47dd2
Luis García R <67235811+lcgr220@users.noreply.github.com>
2021-06-22 17:51:46 -0400

    Tarea Formulario y el DOM
    Tarea Formulario y el DOM

---
+++ Clase #7/main.js
@@ -0,0 +1,13 @@
+function cambiarletra(){
+    document.getElementById("caja").innerHTML = "<p>Valor etiqueta</p>";
+}
+
+function cambiarfondo(){
+    document.getElementById("caja").style.background ="PURPLE";
+}
+function cambiarfuente(){
+    document.getElementById("caja").style.fontSize = "25px";
+    document.getElementById("caja").style.width  = "400px";
+    document.getElementById("caja").style.height  = "300px"; 
+    document.getElementById("caja").style.borderTopRightRadius = "10%";
+}
