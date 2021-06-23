efae4bd633b10aa5bd0b49465750f76439babe6b
Luis García R <67235811+lcgr220@users.noreply.github.com>
2021-06-22 20:50:47 -0400

    Actualizacion codigo
    Tarea- DOM & FORM

--- Clase #7/main.js
+++ Clase #7/main.js
@@ -1,13 +1,27 @@
-function cambiarletra(){
-    document.getElementById("caja").innerHTML = "<p>Valor etiqueta</p>";
-}
 
-function cambiarfondo(){
-    document.getElementById("caja").style.background ="PURPLE";
-}
-function cambiarfuente(){
-    document.getElementById("caja").style.fontSize = "25px";
-    document.getElementById("caja").style.width  = "400px";
-    document.getElementById("caja").style.height  = "300px"; 
-    document.getElementById("caja").style.borderTopRightRadius = "10%";
-}
+const boton = document.querySelector("#boton");
+const ancho = document.querySelector(".ancho");
+const altura = document.querySelector(".altura");
+const bcolor = document.querySelector(".color");
+const contenedor = document.querySelector("#contenedor");
+const texto = document.querySelector(".texto");
+const textoDiv = document.querySelector(".texto-div");
+
+boton.addEventListener("click", () => {
+  contenedor.style.width = ancho.value;
+  contenedor.style.height = altura.value;
+  contenedor.style.backgroundColor = bcolor.value;
+  contenedor.style.padding = "0 15px";
+  contenedor.style.overflowY = "scroll";
+  textoDiv.innerHTML = texto.value;
+  contenedor.style.color = "white";
+  contenedor.style.display = "block";
+  contenedor.style.lineHeight = "1.5";
+  contenedor.style.transition = "all 2s";
+
+  if (contenedor.style.backgroundColor == "white") {
+    contenedor.style.color = "black";
+  } else if (contenedor.style.backgroundColor == "") {
+    contenedor.style.color = "black";
+  }
+});
